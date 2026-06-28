432c52e40d049261fb85dffa2dffae7530edc267
sakshijaiswal321 <sakshijaiswal1233@gmail.com>
2026-06-28 16:49:57 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value.trim() === "Back") {
+    display.value = display.value.slice(0, -1);
+
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            }
+            catch {
+                display.value = "Error";
+            }
+        }
+        else {
+            display.value += value;
+        }
+    });
+});
